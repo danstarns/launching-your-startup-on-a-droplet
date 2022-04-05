254afc4d9df1f9dbf68ae9f227e0cff7c26f81e8
Daniel Starns <danielstarns@hotmail.com>
2022-04-05 18:10:05 +0000

    fix: path for build

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { HTTP_PORT = 5000, STATIC_FOLDER = "../../client/build" } = process.env;
+const { HTTP_PORT = 5000, STATIC_FOLDER = "../client/build" } = process.env;
 
 module.exports = {
   HTTP_PORT,
