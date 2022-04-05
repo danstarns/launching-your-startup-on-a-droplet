7e64a07b14d3fb27f2d17230d6ce33360ba102fc
Daniel Starns <danielstarns@hotmail.com>
2022-04-05 09:19:31 +0000

    feat: add simple frontend to support querying server

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+async function getUsers() {
+  const baseURL = process.env.REACT_APP_API_URL || "/";
+
+  const response = await fetch(`${baseURL}/users`);
+
+  if (!response.ok) {
+    throw new Error(await response.text());
+  }
+
+  const users = await response.json();
+
+  return users;
+}
+
+export function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setUsers(await getUsers());
+    })();
+  }, []);
+
+  return (
+    <div className="users">
+      <h1>Users</h1>
+      <div>
+        {users.map((user) => (
+          <div key={user.name}>
+            <h2>{user.name}</h2>
+            <a href={user.website}>{user.website}</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
