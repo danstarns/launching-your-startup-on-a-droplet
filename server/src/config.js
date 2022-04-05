84e390b849b36fd3cfd717cebe8123ffa1ae1106
Daniel Starns <danielstarns@hotmail.com>
2022-04-05 17:27:22 +0000

    config: *

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { HTTP_PORT, STATIC_FOLDER } = process.env;
+const { HTTP_PORT = 5000, STATIC_FOLDER = "../../client/build" } = process.env;
 
 module.exports = {
   HTTP_PORT,
